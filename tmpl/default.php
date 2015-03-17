6248772a906d6369fc6cafe301b56bd883689c50
Rogier <rogier@spreadit.nl>
2015-03-17 14:54:16 +0100

    Made the indicators dynamic, and added a check so empty html will not be shown.

--- tmpl/default.php
+++ tmpl/default.php
@@ -16,17 +16,21 @@ defined('_JEXEC') or die;
  <div id="myCarousel" class="carousel slide">
   <!-- Indicators -->
   <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
+  <?php foreach ($slideritems as $key => $item) { 
+      $active = ($key == 1)? " active" : ""; ?>
+      <?php if ($item->main_image) : ?>
+    <li data-target="#myCarousel" data-slide-to="<?php echo ($key - 1); ?>" class="<?php echo $active; ?>"></li>
+    <?php endif; ?>
+  <?php } ?>
   </ol>
 
   <!-- Wrapper for slides -->
-  <div class="carousel-inner container">
+  <div class="carousel-inner">
     <?php foreach ($slideritems as $key => $item) {
       $active = ($key == 1)? " active" : ""; ?>
+      <?php if ($item->main_image) : ?>
       <div class="item<?php echo $active; ?>">
-        <div class="carousel-caption col-md-5 col-md-offset-1">
+        <div class="carousel-caption">
             <h1 class="carousel-title"><?php echo $item->heading; ?></h1>
             <?php if($item->text != "no") : ?>
             <p class="carousel-body"><?php echo $item->text; ?></p>
@@ -36,11 +40,12 @@ defined('_JEXEC') or die;
         <?php endif; ?>
     </div>
         <?php if($item->main_image) : ?>
-          <div class="carousel-image col-md-12">
+          <div class="carousel-image">
              <?php if($item->button_link) : ?><a href="<?php echo $item->button_link; ?>"><?php endif; ?><img class="server" src="<?php echo JURI::base(); ?><?php echo $item->main_image; ?>" alt="<?php echo($item->heading && $item->heading != "Slide " . $key . " Heading" ? $item->heading : ""); ?>" /><?php if($item->button_link) : ?></a><?php endif; ?>
     </div>
       <?php endif; ?>
     </div>
+      <?php endif; ?>
     <?php } ?>
     </div>
 
