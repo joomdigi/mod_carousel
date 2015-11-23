d19f7b8c50345a80c8a8bff94fc49599def7923a
Peter Vavro <peter@vavro.me>
2015-11-22 20:47:33 -0500

    Code Update + Option to carousel element id added
    - strange code removed
    - code for "Slide Caption Class" added
    - input for "Slide Caption Class" moved
    - Option to change carousel element id added

--- mod_carousel.php
+++ mod_carousel.php
@@ -16,12 +16,13 @@ $moduleclass_sfx = htmlspecialchars($params->get('moduleclass_sfx'));
 //store params as object that we can loop through in tmpl/default.php
 $slideritems = new stdClass;
 
-<<<<<<< HEAD
-for ($i=1; $i <= 3; $i++) { 
-        $slideritems->$i = new stdClass;
-=======
+$carousel_id = $params->get('carousel_id', 'myCarousel');
+
+
+for ($i=1; $i <= 6; $i++) $slideritems->$i = new stdClass;
+
 for ($i=1; $i <= 6; $i++) { 
->>>>>>> 6248772a906d6369fc6cafe301b56bd883689c50
+
 	$slideritems->$i->heading = $params->get("slide".$i."_heading");
 	$slideritems->$i->text = $params->get("slide".$i."_text");
 	$slideritems->$i->background_image = $params->get("slide".$i."_background_image");
@@ -29,13 +30,11 @@ for ($i=1; $i <= 6; $i++) {
 	$slideritems->$i->button_text = $params->get("slide".$i."_button_text");
 	$slideritems->$i->button_link = $params->get("slide".$i."_button_link");
 	$slideritems->$i->show_read_more = $params->get("slide".$i."_show_read_more");
+	$slideritems->$i->caption_class = $params->get("slide".$i."_caption_class");
 }
 
 //more options
 $custom_style = $params->get('custom_style');
-$slide1_caption_class = $params->get('slide1_caption_class');
-$slide2_caption_class = $params->get('slide2_caption_class');
-$slide3_caption_class = $params->get('slide3_caption_class');
 
 // Include the syndicate functions only once
 require_once dirname(__FILE__).'/helper.php';
